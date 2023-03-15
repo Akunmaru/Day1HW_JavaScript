543143a3f8fe2671907f59f0c185bf87d73cd7ac
Akunmaru <augustineagena@gmail.com>
2023-03-15 12:08:22 -0600

    Added the 'Codewars' folder

---
+++ Codewars/function2_squaringAnArguement_8kyu.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Now you have to write a function that takes an argument and returns the square of it.
+
+//My Solution
+// Write the "square"-function here
+let square = function(number) {
+    var sqr = number ** 2
+    return sqr
+  }
